12e036cae90253a0cab5933d31b3b2ce306da4d3
Brett Olsen <brett.olsen@gmail.com>
2017-10-03 21:23:33 -0700

    Add venv display, clean things up

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-function set_prompt
-{
-local ESC_OPN="\[\033["
-local ESC_CLS="\]"
+GREY="\[\033[1;37m\]"
+GREEN="\[\033[1;32m\]"
+RED="\[\033[1;31m\]"
+YELLOW="\[\033[1;33m\]"
+BLUE="\[\033[1;34m\]"
+MAGENTA="\[\033[1;35m\]"
+CYAN="\[\033[1;36m\]"
+NORMAL="\[\033[0m\]"
 
-local GREY="\[\033[1;37m\]"
-local GREEN="\[\033[1;32m\]"
-local RED="\[\033[1;31m\]"
-local YELLOW="\[\033[1;33m\]"
-local BLUE="\[\033[1;34m\]"
-local MAGENTA="\[\033[1;35m\]"
-local CYAN="\[\033[1;36m\]"
-local NORMAL="\[\033[0m\]"
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
+function prompt() {
 
-function git_line
-{
-  local BRANCH="\$(git branch | grep ^* | sed s/\*\ //)"
-  local ORIGIN="\$(git remote -v | grep '(fetch)' | sed 's/.*git@//' | sed 's/\.git.*//')"
-  local LINE="${YELLOW}---git:($CYAN${ORIGIN}$YELLOW - $MAGENTA$BRANCH$YELLOW)---\n$NORMAL"
-  echo "\$(git branch &> /dev/null; if [ \$? -eq 0 ]; then echo "\"$LINE\""; fi)"
-}
+    function git_info() {
+        local BRANCH="\$(git branch | grep ^* | sed s/\*\ //)"
+        local ORIGIN="\$(git remote -v | grep '(fetch)' | sed 's/.*git@//' | sed 's/\.git.*//')"
+        local LINE="${YELLOW}---git:($CYAN${ORIGIN}$YELLOW - $MAGENTA$BRANCH$YELLOW)---\n$NORMAL"
+        echo "\$(git branch &> /dev/null; if [ \$? -eq 0 ]; then echo "\"$LINE\""; fi)"
+    }
 
-export PS1="$GREY(\$(date +'%b %d %Y'), \@) - ${BLUE}\u${CYAN}@${YELLOW}\h ${GREEN}\w\n$(git_line)${YELLOW}\$>${NORMAL} "
+    function virtualenv_info() {
+        local LINE="$YELLOW--venv:($CYAN\${VIRTUAL_ENV##*/}$YELLOW)--\n$NORMAL"
+        echo "\$(if [[ -n \$VIRTUAL_ENV ]]; then echo "\"$LINE\""; fi)"
+    }
+
+    export PS1="$GREY(\$(date +'%b %d %Y'), \@) - \
+${BLUE}\u${CYAN}@${YELLOW}\h \
+${GREEN}\w\n\
+$(git_info)$(virtualenv_info)\
+${YELLOW}\$>${NORMAL} "
 }
